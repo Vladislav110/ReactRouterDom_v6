8a1c5aa066d2a25bfc47c6d7c763f110096488d1
Vlad Matsuk <matsukvlad399@gmail.com>
2022-10-18 16:43:40 +0300

    Add ReactRouterDom lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes, Outlet, useParams, useNavigate} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const Profile = () => {
+//     const params = useParams<"id">()
+//     const some = params.id
+//     console.log(some)
+//
+//     return (
+//         <div>Profile info</div>
+//     )
+// }
+//
+//
+// function App() {
+//     return (
+//         <div className="App">
+//             <NavLink to={"/"}>Main</NavLink>---
+//             <NavLink to={"/login"}>Login</NavLink>---
+//             <NavLink to={"/profile/1"}>Profile</NavLink>---
+//
+//
+//             <Routes>
+//                 <Route path={"/*"} element={<div>404</div>}/>
+//                 <Route path={"/"} element={<div>main</div>}/>
+//                 <Route path={"/login"} element={<div>login</div>}/>
+//                 <Route path={"/profile/:id"} element={<Profile/>}/>
+//             </Routes>
+//         </div>
+//     )
+// }
+const Profile = ()=> {
+    const navigate = useNavigate()
+
+    return(
+        <div>
+            profile
+            <button onClick={()=>{navigate(-1)}}>LogOut</button>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="App">
+            <NavLink to={"/"}>Main</NavLink>---
+            <NavLink to={"/login"}>Login</NavLink>---
+            <NavLink to={"/profile/1"}
+                     style={(params) => {
+                         return {color: params.isActive ? 'lime' : 'blue'}
+                     }}
+            >Profile
+            </NavLink>---
+            <NavLink to={"/profile/settings"}
+                     className={(isActive) => isActive ? 'act' : 'def'}
+
+            >Settings</NavLink>---
+
+            <a
+            href={"https://google.com"}
+            target={'_blank'} // открывает в новом окне
+                rel={'noreferrer nofollow noopener'} // для безопасности
+            >Google</a>
+
+
+            <Routes>
+                <Route path={"/*"} element={<div>404</div>}/>
+                <Route path={"/"} element={<div>main</div>}/>
+                <Route path={"/login"} element={<div>login</div>}/>
+                <Route path={"/profile/:id"} element={<Profile/>}/>
+                <Route path={"/profile/settings"} element={<div>settings</div>}/>
+            </Routes>
+
+        </div>
+    )
 }
 
 export default App;
